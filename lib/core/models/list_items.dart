db2c23e5c33d7e635b1329100842b2fd891e7bb1
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2020-08-19 13:58:38 -0300

    Home screen added list

---
+++ lib/core/models/list_items.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListItem {
+  String name;
+  double value;
+  int id;
+  bool isChecked;
+
+  ListItem({
+    @required this.name,
+    this.isChecked = false,
+    this.value,
+    this.id,
+  });
+}
--- lib/core/view_models/main_view_model.dart
+++ lib/core/view_models/main_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:calcu_list/core/models/list_items.dart';
 import 'package:calcu_list/core/view_models/base_view_model.dart';
 
 class MainViewModel extends BaseViewModel {
@@ -8,5 +9,49 @@ class MainViewModel extends BaseViewModel {
       _mainViewModel = MainViewModel();
     }
     return _mainViewModel;
+  }
+
+  // Variables
+
+  List<ListItem> listItems = List();
+
+  // Constructor
+  MainViewModel() {
+    listItems = [
+      ListItem(name: "Frango"),
+      ListItem(name: "Arroz"),
+      ListItem(name: "Feijão"),
+      ListItem(name: "Macarrão"),
+      ListItem(name: "Batata"),
+      ListItem(name: "Maçã"),
+      ListItem(name: "Coquinha gelada"),
+    ];
+  }
+  //////////////
+
+  // Functions
+
+  void addItem(String name) {
+    listItems.add(ListItem(name: name, id: listItems.length));
+    notifyListeners();
+  }
+
+  void removeItem(int index) {
+    listItems.removeAt(index);
+    notifyListeners();
+  }
+
+  void editValue(String name, double value, int index) {
+    listItems[index] = ListItem(
+      name: name,
+      value: value,
+      id: index,
+    );
+    notifyListeners();
+  }
+
+  void checkItem(int index) {
+    listItems[index].isChecked = !listItems[index].isChecked;
+    notifyListeners();
   }
 }
