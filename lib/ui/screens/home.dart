373420d2dd35f745652873f3ded3a7bf0ded5cea
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2020-08-19 15:19:07 -0300

    Home screen linked with backend

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -9,62 +9,63 @@ class HomeScreen extends StatelessWidget {
     final mainViewModel = Provider.of<MainViewModel>(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Calcu List"),
-          centerTitle: true,
-        ),
-        body: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: mainViewModel.listItems.length,
-              itemBuilder: (BuildContext context, int index) => Dismissible(
-                // confirmDismiss: (direction) async {
-                //   if (direction == DismissDirection.startToEnd) {
-                //     return true;
-                //   } else {
-                //     return false;
-                //   }
-                // },
-                background: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.black12,
-                ),
-                key: ValueKey("Item-$index"),
-                child: ListTile(
-                  leading: GestureDetector(
-                    onTap: () => mainViewModel.checkItem(index),
-                    child: Image(
-                      image: mainViewModel.listItems[index].isChecked
-                          ? AssetImage("assets/images/completed.png")
-                          : AssetImage("assets/images/non-completed.png"),
-                    ),
-                  ),
-                  title: Text(
-                    "Item $index",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  subtitle: Text(r"Valor R$"),
-                  onTap: () => showDialog(
-                    context: context,
-                    builder: (context) => AddPopup(
-                      index: index,
-                      title: "Title $index",
-                    ),
-                  ),
-                ),
-                direction: DismissDirection.startToEnd,
-                onDismissed: (direction) {
-                  print('Dismissed $direction');
-                },
+      appBar: AppBar(
+        title: Text("Calcu List"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: mainViewModel.listItems.length,
+            itemBuilder: (BuildContext context, int index) => Dismissible(
+              // confirmDismiss: (direction) async {
+              //   if (direction == DismissDirection.startToEnd) {
+              //     return true;
+              //   } else {
+              //     return false;
+              //   }
+              // },
+              background: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.black12,
               ),
+              key: ValueKey("Item-$index"),
+              child: ListTile(
+                leading: GestureDetector(
+                  onTap: () => mainViewModel.checkItem(index),
+                  child: Image(
+                    image: mainViewModel.listItems[index].isChecked
+                        ? AssetImage("assets/images/completed.png")
+                        : AssetImage("assets/images/non-completed.png"),
+                  ),
+                ),
+                title: Text(
+                  "Item $index",
+                  style: TextStyle(color: Colors.black),
+                ),
+                subtitle: Text(r"Valor R$"),
+                onTap: () => showDialog(
+                  context: context,
+                  builder: (context) => AddPopup(
+                    index: index,
+                    title: "Title $index",
+                  ),
+                ),
+              ),
+              direction: DismissDirection.startToEnd,
+              onDismissed: (direction) {
+                print('Dismissed $direction');
+              },
             ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: [
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.ac_unit), title: Text("asd")),
           BottomNavigationBarItem(
@@ -73,6 +74,8 @@ class HomeScreen extends StatelessWidget {
               icon: Icon(Icons.ac_unit), title: Text("asd")),
           BottomNavigationBarItem(
               icon: Icon(Icons.ac_unit), title: Text("asd")),
-        ]));
+        ],
+      ),
+    );
   }
 }
