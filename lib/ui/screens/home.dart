818dedcc71f006f4ac5b7b5aea23e202c525692d
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2020-08-19 14:45:51 -0300

    Home screen linked with backend

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,9 +1,13 @@
+import 'package:calcu_list/core/view_models/main_view_model.dart';
 import 'package:calcu_list/ui/widgets/add_popup.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final mainViewModel = Provider.of<MainViewModel>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Calcu List"),
@@ -15,7 +19,7 @@ class HomeScreen extends StatelessWidget {
           ListView.builder(
             shrinkWrap: true,
             physics: NeverScrollableScrollPhysics(),
-            itemCount: 10,
+            itemCount: mainViewModel.listItems.length,
             itemBuilder: (BuildContext context, int index) => Dismissible(
               // confirmDismiss: (direction) async {
               //   if (direction == DismissDirection.startToEnd) {
@@ -31,8 +35,14 @@ class HomeScreen extends StatelessWidget {
               ),
               key: ValueKey("Item-$index"),
               child: ListTile(
-                leading:
-                    Image(image: AssetImage("assets/images/non-completed.png")),
+                leading: GestureDetector(
+                  onTap: () => mainViewModel.checkItem(index),
+                  child: Image(
+                    image: mainViewModel.listItems[index].isChecked
+                        ? AssetImage("assets/images/completed.png")
+                        : AssetImage("assets/images/non-completed.png"),
+                  ),
+                ),
                 title: Text(
                   "Item $index",
                   style: TextStyle(color: Colors.black),
