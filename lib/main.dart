c965a5ac2863b32745828d1e43f90b3ffff67ea5
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2020-08-19 13:16:24 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:calcu_list/core/view_models/main_view_model.dart';
+import 'package:calcu_list/ui/values/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:calcu_list/ui/screens/home.dart';
 import 'package:calcu_list/ui/values/routes.dart' as Routes;
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: APP_NAME,
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.deepPurple),
         home: HomeScreen(),
--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -5,6 +5,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) => Dismissible(
+              // confirmDismiss: (direction) {
+              //   if (direction == DismissDirection.startToEnd) {
+              //     return true;
+              //   }
+              // },
+              key: ValueKey("Item-$index"),
+              child: ListTile(
+                leading: CircleAvatar(),
+                title: Text(
+                  "Title",
+                  style: TextStyle(color: Colors.black),
+                ),
+                subtitle: Text("value"),
+              ),
+              direction: DismissDirection.startToEnd,
+              onDismissed: (direction) {
+                print('Dismissed $direction');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
